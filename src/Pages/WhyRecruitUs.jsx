d20eaf57da85d4c44109a0b5594c804e622cae57
Abhijeet <abhijeetwankhade2001@gmail.com>
2021-05-08 22:57:25 +0530

    tp

--- src/Pages/WhyRecruitUs.jsx
+++ src/Pages/WhyRecruitUs.jsx
@@ -2,8 +2,35 @@ import React from 'react'
 
 export default function whyRecruitUs() {
     return (
-        <div>
-            <h1>This is whyRecruitUs</h1>
+        <div className="recruitUs">
+            <div className="recruitUsBlock">
+                <h3 className="recruitUsHeading"> The Prodigious Curriculum and Academic System </h3>
+                <div className="recruitUsCard"> 
+                    Being mentored by IITH, IIITR has adopted the innovative
+                    fractal academics system, which warrants continuous
+                    evaluation. This ensures the students have a deeper
+                    understanding of the core subjects. Moreover, the highly
+                    flexible curriculum forged by the esteemed faculty of IITH and
+                    IIITR exuberates innovation and inventiveness. With core
+                    subjects being incorporated from the first semester itself, the
+                    students are able to finish their courses quite early and focus
+                    on developing projects & industry relevant skills.
+                </div>
+            </div>
+            <div className="recruitUsBlock">
+                <div className="recruitUsCard"> 
+                    The student family of IIITR consists of competitive coders, web development enthusiasts, eager learners and diligent scholars.
+                    The students represent the creamy layer of talent in the country.
+                    They have been actively involved in various technical and cultural
+                    club activities of IIT Hyderabad. At the same time, the students
+                    have founded and set up numerous clubs of their own to further
+                    facilitate collaborative learning and introspective growth. (The
+                    students have been exploring various industry relevant skills &
+                    technologies like Python, C++, Java, Javascript, flutter, Go, flask,
+                    NoSQL, SQL, Django, Node, Vue, React, Docker etc.)
+                </div>
+                <h3 className="recruitUsHeading"> The coders, thinkers and tinkerers </h3>
+            </div>
         </div>
     )
 }
