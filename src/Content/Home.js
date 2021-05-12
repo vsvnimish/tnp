4be9d5c62e3b2700efa4a3e1689d224f066c8c0d
Abhijeet <abhijeetwankhade2001@gmail.com>
2021-05-13 00:52:49 +0530

    fixed long string

--- src/Content/Home.js
+++ src/Content/Home.js
@@ -1,8 +1,8 @@
 const AboutIIITR =
-  "The Indian Institute of Information Technology, Raichur(IIITR) was established in 2019, as a centre of academic excellence in the field of Computer Science and Information Technology. Currently being mentored by the Indian Institute of Technology, Hyderabad(IITH), IIITR is an institute of national importance, formed under the Public Private Partnership model, with contributions from MHRD, Government of India and the Government of Karnataka.\
-With its highly esteemed & able faculty, modern curriculum and a pioneering batch of bright minded individuals, IIITR is an abode of creativity and innovation, ready to show the world what it is capable of.";
+  `The Indian Institute of Information Technology, Raichur(IIITR) was established in 2019, as a centre of academic excellence in the field of Computer Science and Information Technology. Currently being mentored by the Indian Institute of Technology, Hyderabad(IITH), IIITR is an institute of national importance, formed under the Public Private Partnership model, with contributions from MHRD, Government of India and the Government of Karnataka.
+With its highly esteemed & able faculty, modern curriculum and a pioneering batch of bright minded individuals, IIITR is an abode of creativity and innovation, ready to show the world what it is capable of.`;
 
 const Mission =
-  "Budding under one of the most sought after institutes in the country, IIITR has had the philosophy of bringing positive changes in the lives of its students, and thereby, the society at large, from its very inception. The mission has always been to cultivate ingenuity, originality and innovation in the students, who are the future of this country.";
+  `Budding under one of the most sought after institutes in the country, IIITR has had the philosophy of bringing positive changes in the lives of its students, and thereby, the society at large, from its very inception. The mission has always been to cultivate ingenuity, originality and innovation in the students, who are the future of this country.`;
 
 export { AboutIIITR, Mission };
