4eee7b6d2793d747dfbe0c2554fc6ede47a57353
abhijeet <abhijeetwankhade2001@gmail.com>
2021-05-13 20:27:05 +0530

    formatted content

--- src/Content/Home.js
+++ src/Content/Home.js
@@ -5,4 +5,76 @@ With its highly esteemed & able faculty, modern curriculum and a pioneering batc
 const Mission =
   `Budding under one of the most sought after institutes in the country, IIITR has had the philosophy of bringing positive changes in the lives of its students, and thereby, the society at large, from its very inception. The mission has always been to cultivate ingenuity, originality and innovation in the students, who are the future of this country.`;
 
-export { AboutIIITR, Mission };
+
+const Thoughts = [
+  {
+    id: 1,
+    isLeftAlign: true,
+    haveLogo: true,
+    writer: "BS Murty",
+    heading: "From Director's Desk",
+    content:  `Being mentored by IITH, IIITR has adopted the innovative
+    fractal academics system, which warrants continuous
+    evaluation. This ensures the students have a deeper
+    understanding of the core subjects. Moreover, the highly
+    flexible curriculum forged by the esteemed faculty of IITH and
+    IIITR exuberates innovation and inventiveness. With core
+    subjects being incorporated from the first semester itself, the
+    students are able to finish their courses quite early and focus
+    on developing projects & industry relevant skills.`,
+    imgSrc: "https://images-ext-1.discordapp.net/external/1CnMkqZMSH_K0lNVZPLhboBu-8x6JSb5Hixk0HRsnvw/https/ocs.iith.ac.in/static/media/directorsdesk.bb13dd37.jpg?width=501&height=450",
+    altText: "Director"
+  },
+  {
+    id: 2,
+    isLeftAlign: false,
+    haveLogo: true,
+    writer: "Sadhna Jha",
+    heading: "Message From TPO",
+    content:  `Being mentored by IITH, IIITR has adopted the innovative
+    fractal academics system, which warrants continuous
+    evaluation. This ensures the students have a deeper
+    understanding of the core subjects. Moreover, the highly
+    flexible curriculum forged by the esteemed faculty of IITH and
+    IIITR exuberates innovation and inventiveness. With core
+    subjects being incorporated from the first semester itself, the
+    students are able to finish their courses quite early and focus
+    on developing projects & industry relevant skills.`,
+    imgSrc: "https://images-ext-1.discordapp.net/external/Eld7F0Wj94n5lzFm6tRzx6gd96ikkZX1a75dfitylIU/https/iiitr.ac.in/assets/images/faculty/cse/sadhana.webp",
+    altText: "tpo"
+  },
+]
+
+
+const WhyUs = [
+  {
+    id: 1,
+    heading: "The Prodigious Curriculum and Academic System" ,
+    content: ` Being mentored by IITH, IIITR has adopted the innovative
+                    fractal academics system, which warrants continuous
+                    evaluation. This ensures the students have a deeper
+                    understanding of the core subjects. Moreover, the highly
+                    flexible curriculum forged by the esteemed faculty of IITH and
+                    IIITR exuberates innovation and inventiveness. With core
+                    subjects being incorporated from the first semester itself, the
+                    students are able to finish their courses quite early and focus
+                    on developing projects & industry relevant skills.`,
+  },
+  {
+    id: 2,
+    heading: "The coders, thinkers and tinkerers" ,
+    content: `The student family of IIITR consists of competitive coders, 
+                        web development enthusiasts, eager learners and diligent scholars.
+                        The students represent the creamy layer of talent in the country.
+                        They have been actively involved in various technical and cultural
+                        club activities of IIT Hyderabad. At the same time, the students
+                        have founded and set up numerous clubs of their own to further
+                        facilitate collaborative learning and introspective growth. (The
+                        students have been exploring various industry relevant skills &
+                        technologies like Python, C++, Java, Javascript, flutter, Go, flask,
+                        NoSQL, SQL, Django, Node, Vue, React, Docker etc.)`,
+  }
+]
+
+
+export { AboutIIITR, Mission, Thoughts , WhyUs};
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,6 +1,5 @@
-
 import React from 'react'
-import {AboutIIITR} from '../Content/Home'
+import {AboutIIITR, Thoughts, WhyUs} from '../Content/Home.js'
 import {ReactComponent as Logo} from "../images/ocs_logo.svg"
 import ReusableCard from '../components/ReusableBlock';
 import ReusableBlock2 from '../components/ReusableBlock2';
@@ -60,41 +59,20 @@ export default function HomePage() {
                 <h1>About Us</h1>
                 <p>{AboutIIITR}</p>
             </div>
-            <ReusableCard 
-                isLeftAlign={true}
-                haveLogo={true}
-                writer="BS Murty"
-                heading="From Director's Desk"
-                content=" Being mentored by IITH, IIITR has adopted the innovative
-                    fractal academics system, which warrants continuous
-                    evaluation. This ensures the students have a deeper
-                    understanding of the core subjects. Moreover, the highly
-                    flexible curriculum forged by the esteemed faculty of IITH and
-                    IIITR exuberates innovation and inventiveness. With core
-                    subjects being incorporated from the first semester itself, the
-                    students are able to finish their courses quite early and focus
-                    on developing projects & industry relevant skills."
-                imgSrc="https://images-ext-1.discordapp.net/external/1CnMkqZMSH_K0lNVZPLhboBu-8x6JSb5Hixk0HRsnvw/https/ocs.iith.ac.in/static/media/directorsdesk.bb13dd37.jpg?width=501&height=450"
-                altText="Director"
-            />
-                <ReusableCard 
-                isLeftAlign={false}
-                haveLogo={true}
-                heading="Message From TPO"
-                writer="Sadhna Jha"
-                content=" Being mentored by IITH, IIITR has adopted the innovative
-                    fractal academics system, which warrants continuous
-                    evaluation. This ensures the students have a deeper
-                    understanding of the core subjects. Moreover, the highly
-                    flexible curriculum forged by the esteemed faculty of IITH and
-                    IIITR exuberates innovation and inventiveness. With core
-                    subjects being incorporated from the first semester itself, the
-                    students are able to finish their courses quite early and focus
-                    on developing projects & industry relevant skills."
-                imgSrc="https://images-ext-1.discordapp.net/external/Eld7F0Wj94n5lzFm6tRzx6gd96ikkZX1a75dfitylIU/https/iiitr.ac.in/assets/images/faculty/cse/sadhana.webp"
-                altText="tpo"
-            />
 
+            {Thoughts.map((item)=>{
+                return(
+                    <ReusableCard
+                        isLeftAlign={item.isLeftAlign}
+                        haveLogo={item.haveLogo}
+                        heading={item.heading}
+                        writer={item.writer}
+                        content={item.content}
+                        imgSrc={item.imgSrc}
+                        altText={item.altText}
+                    />
+                )
+            })}
  
         </div>
         <div className="whyUsHeading">
@@ -102,32 +80,14 @@ export default function HomePage() {
         </div>
 
         <div className="whyUsContent">
-            <ReusableBlock2 
-                heading="The Prodigious Curriculum and Academic System"
-                content=" Being mentored by IITH, IIITR has adopted the innovative
-                    fractal academics system, which warrants continuous
-                    evaluation. This ensures the students have a deeper
-                    understanding of the core subjects. Moreover, the highly
-                    flexible curriculum forged by the esteemed faculty of IITH and
-                    IIITR exuberates innovation and inventiveness. With core
-                    subjects being incorporated from the first semester itself, the
-                    students are able to finish their courses quite early and focus
-                    on developing projects & industry relevant skills."
-            />
-
-            <ReusableBlock2 
-                heading="The coders, thinkers and tinkerers"
-                content="The student family of IIITR consists of competitive coders, 
-                        web development enthusiasts, eager learners and diligent scholars.
-                        The students represent the creamy layer of talent in the country.
-                        They have been actively involved in various technical and cultural
-                        club activities of IIT Hyderabad. At the same time, the students
-                        have founded and set up numerous clubs of their own to further
-                        facilitate collaborative learning and introspective growth. (The
-                        students have been exploring various industry relevant skills &
-                        technologies like Python, C++, Java, Javascript, flutter, Go, flask,
-                        NoSQL, SQL, Django, Node, Vue, React, Docker etc.)"
-            />
+            {WhyUs.map((item)=>{
+                return(
+                    <ReusableBlock2
+                        heading={item.heading}
+                        content={item.content}
+                    />
+                )
+            })}
         </div>
             {/* <div className="content">
 	            <div>
