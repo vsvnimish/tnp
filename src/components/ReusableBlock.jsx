e099e66d34751e9efc5c96cd3dec34c50a4d9430
Vibhanshu Jain <cs19b1027@iiitr.ac.in>
2021-05-27 23:26:39 +0530

    Shadow cards added

--- src/components/ReusableBlock.jsx
+++ src/components/ReusableBlock.jsx
@@ -1,49 +1,56 @@
-import React from "react"
+import React from "react";
 
-function ReusableCard(props){
-
-    return (
-      props.isLeftAlign ? <div className="reusableMain" data-aos="fade-up" >
-
-      <div className="head" >
-        <h3 className="reusableHeading"> {props.heading}  </h3>
+function ReusableCard(props) {
+  return props.isLeftAlign ? (
+    <div className="reusableMain" data-aos="fade-up">
+      <div className="head">
+        <h3 className="reusableHeading"> {props.heading} </h3>
         <hr className="lineBreak" />
       </div>
-      <div className="reusableBlock" >
-
-      <div className="reusableHeadingBlock">
-        {props.haveLogo ? <img className="reusableHeadingImage" src={props.imgSrc} alt={props.altText} /> : null } 
+      <div className="reusableBlock indexRecruitUs">
+        <div className="reusableHeadingBlock">
+          {props.haveLogo ? (
+            <img
+              className="reusableHeadingImage"
+              src={props.imgSrc}
+              alt={props.altText}
+            />
+          ) : null}
+        </div>
+        <div className="reusableCard">
+          <blockquote>
+            <p className="quotation"> {props.content}</p>
+            <footer> {props.writer}</footer>
+          </blockquote>
+        </div>
       </div>
-      <div className="reusableCard"> 
-        <blockquote><p className="quotation" > {props.content}</p>
-          <footer> {props.writer}</footer>
-        </blockquote>
-      </div>
-
-      </div>
-      
-      </div>
-
-      :   <div className="reusableMain" data-aos="fade-up" >
-
-        <div className="head rightHead" >
-        <h3 className="reusableHeading"> {props.heading}  </h3>
+    </div>
+  ) : (
+    <div className="reusableMain" data-aos="fade-up">
+      <div className="head rightHead">
+        <h3 className="reusableHeading"> {props.heading} </h3>
         <hr className="lineBreak" />
       </div>
 
-        <div className="reusableBlock" >
-      <div className="reusableCard"> 
-        <blockquote className="leftBlock" ><p className="quotation"> {props.content}</p>
-          <footer> {props.writer}</footer>
-        </blockquote>
+      <div className="reusableBlock indexRecruitUs">
+        <div className="reusableCard">
+          <blockquote className="leftBlock">
+            <p className="quotation"> {props.content}</p>
+            <footer> {props.writer}</footer>
+          </blockquote>
+        </div>
+        <div className="reusableHeadingBlock ">
+          {props.haveLogo ? (
+            <img
+              className="reusableHeadingImage"
+              src={props.imgSrc}
+              alt={props.altText}
+            />
+          ) : null}
+        </div>
       </div>
-      <div className="reusableHeadingBlock">
-        {props.haveLogo ? <img className="reusableHeadingImage" src={props.imgSrc} alt={props.altText} /> : null } 
-      </div>
-
-      </div>
-  </div>
-    );
+    </div>
+  );
 }
 
 export default ReusableCard;
