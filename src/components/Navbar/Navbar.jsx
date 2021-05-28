15d052268ac074657ed3403e408067733885c59d
abhijeet <abhijeetwankhade2001@gmail.com>
2021-05-28 14:32:11 +0530

    awsdx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -110,15 +110,15 @@ const Navbar = React.forwardRef((props, ref) => {
     setOpen(false);
   };
 
-  function isInViewport(element) {
-    const rect = element.getBoundingClientRect();
-    return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
-}
+//   function isInViewport(element) {
+//     const rect = element.getBoundingClientRect();
+//     return (
+//         rect.top >= 0 &&
+//         rect.left >= 0 &&
+//         rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+//         rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+//     );
+// }
 
   const changeNavBack = () => {
     if (window.scrollY > 20) {
