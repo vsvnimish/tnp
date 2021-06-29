bf1b27e2bd775abf8e2f38f0fb583f5f500cdbf1
Aditya Agrawal <adiag1200@gmail.com>
2021-06-29 15:03:22 +0530

    add: Dr. Sadhana Jha, emails

--- src/Content/footerInfo.js
+++ src/Content/footerInfo.js
@@ -4,20 +4,20 @@ const address = `Government Engineering College, Raichur, located at Yeramarus.`
 const Contacts = [
     {
         id: 1,
-        name: "Sadhana Jha (Faculty Incharge)",
-        email: "*Placement cell mail ID*",
+        name: "Dr. Sadhana Jha (Faculty Incharge)",
+        email: "fic.placement@iiitr.ac.in",
         phoneNum: "+91 8768779475",
     },
     {
         id: 2,
         name: "Aditya Agrawal (Student Coordinator)",
-        email: "*Placement cell mail ID*",
+        email: "tnp@iiiitr.ac.in",
         phoneNum: "+91 9785559273",
     },
     {
         id: 3,
         name: "Reethu Sanagala (Student Coordinator)",
-        email: "*Placement cell mail ID*",
+        email: "tnp@iiitr.ac.in",
         phoneNum: "+91 8919239558",
     },
     {
